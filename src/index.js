472e2eb2af7dabd098caaa9fe1346882442fade0
Falsher <ivan.zzaharov@gmail.com>
2021-06-13 19:36:53 +0300

    git added d

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './sass/main.scss';
-import eventApiService from './js/apiService';
-import modal from './js/modal';
-import searchForm from './js/search-forms';
+// import eventApiService from './js/apiService';
+// import modal from './js/modal';
+// import searchForm from './js/search-forms';
 
-import modalTpl from './temlates/modal.hbs';
-import eventTpl from './temlates/event.hbs';
+// import modalTpl from './temlates/modal.hbs';
+// import eventTpl from './temlates/event.hbs';
 
