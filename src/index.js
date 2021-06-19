1cd742916e4982aa6991e52ba6f6344593bfa182
Falsher <ivan.zzaharov@gmail.com>
2021-06-19 22:46:55 +0300

    git added proekt

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import './sass/main.scss';
-// import eventApiService from './js/apiService';
+// import eventFinder from './js/eventFinder';
 // import modal from './js/modal';
 // import searchForm from './js/search-forms';
-
 // import modalTpl from './temlates/modal.hbs';
-// import eventTpl from './temlates/event.hbs';
+import eventTpl from './temlates/event.hbs';
 
+const input = document.querySelector('.input');
+const listCountries = document.querySelector('.list-allExecutor');
+input.addEventListener('input', funck);
+function funck(event) {
+  const nameCountry = event.target.value;
+  console.log(nameCountry);
+  fetch(
+    `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${nameCountry}&source=universe&countryCode=US&apikey=GRPbY9DCa4eo82JvpPxXBkSp102AnOHv`,
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then(({ _embedded }) => {
+      console.log(_embedded);
+      const infoGroup = groupList(_embedded);
+      listCountries.insertAdjacentHTML('afterbegin', infoGroup);
+      function groupList(_embedded) {
+        return eventTpl(_embedded);
+      }
+    });
+}
